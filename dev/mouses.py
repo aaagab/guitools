ff3e4bbe63813480ca205719854bc8321bfad0c2
gabaaa <not-set>
2018-12-12 09:51:20 -0600

    coded most of the core functionalities

---
+++ dev/mouses.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# author: Gabriel Auger
+# version: 1.0.0-alpha-1544156629
+# name: guitools
+# license: MIT
+
+import sys, os
+import time
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+
+import modules.shell_helpers.shell_helpers as shell
+
+del sys.path[0:2]
+
+import re
+from pprint import pprint
+
+class Mouse(object):
+    def __init__(self):
+        pass
+
+    def get_x(self):
+        return self.get_coords()[0]
+
+    def get_y(self):
+        return self.get_coords()[1]
+
+    def get_coords(self):
+        for line in shell.cmd_get_value("xdotool getmouselocation").splitlines():
+            for elem in line.split():
+                attrs=elem.split(":")
+                if attrs[0] == "x":
+                    mouseX=int(attrs[1])
+                elif attrs[0] == "y":
+                    mouseY=int(attrs[1])
+
+        return [mouseX, mouseY]
+
+    def set_coords(self, x, y):
+        os.system("xdotool mousemove {} {}".format(x, y))
+
+    def click(self, btn_num):
+        os.system("xdotool click --clearmodifiers {}".format(btn_num))
+
+    def left_click(self):
+        self.click(1)
+
+    def middle_click(self):
+        self.click(2)
+
+    def right_click(self):
+        self.click(3)
+
+    def scrollup(self):
+        self.click(4)
+
+    def scrolldown(self):
+        self.click(5)
+
+    def double_click(self):
+        self.click(1)
+        time.sleep(.1)
+        self.click(1)
+
+    def triple_click(self):
+        self.click(1)
+        time.sleep(.1)
+        self.click(1)
+        time.sleep(.1)
+        self.click(1)
--- example.py
+++ example.py
@@ -1,25 +1,117 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
 # version: 1.0.0-alpha-1544156629
-# name: diplay
+# name: guitools
 # license: MIT
 from pprint import pprint
-
 
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
-from display import *
+
+from guitools import *
 from modules.deps.deps import deps
 from modules.json_config.json_config import Json_config
+import time
 del sys.path[0:2]
 
 conf=Json_config()
 deps(conf.data["deps"])
 
-pprint(get_display())
+os.system("xdg-open .")
+time.sleep(1.5)
 
-pprint(Windows().sorted_by_class().filter_desktop().windows)
+window=Windows().get_active()
+print(window.name)
 
+time.sleep(.5)
+window.set_above()
+window.maximize()
+window.focus()
+window.unset_above()
 
+time.sleep(.5)
+window.set_above()
+window.tile("left")
+window.focus()
+window.unset_above()
 
+time.sleep(.5)
+window.set_above()
+window.tile("right")
+window.focus()
+window.unset_above()
+
+time.sleep(.5)
+window.set_above()
+window.minimize()
+window.unset_above()
+
+window=Window(window.hex_id)
+window.print()
+
+time.sleep(1)
+window.set_above()
+window.set_geometry(dict(x=100, y=128, width=200, height=200))
+window.focus()
+window.unset_above()
+
+time.sleep(1)
+window.set_above()
+window.center()
+window.focus()
+window.unset_above()
+
+for monitor in Monitors().monitors:
+    monitor.print()
+    tiles=monitor.get_tiles(2, 2, True, [1,4])
+    for tile in tiles:
+            tile.print()
+            time.sleep(0.5)
+            window.set_above()
+            window.set_geometry(dict(
+                    x=tile.upper_left_x, 
+                    y=tile.upper_left_y, 
+                    width=tile.width, 
+                    height=tile.height)
+            )
+            window.focus()
+            window.unset_above()
+
+for monitor in Monitors().monitors:
+    monitor.print()
+    tiles=monitor.get_tiles(3, 3, False)
+    for tile in tiles:
+            time.sleep(0.5)
+            window.set_above()
+            window.set_geometry(dict(
+                    x=tile.upper_left_x, 
+                    y=tile.upper_left_y, 
+                    width=tile.width, 
+                    height=tile.height)
+            )
+            window.focus()
+            window.unset_above()
+
+windows=Windows()
+windows.print()
+print()
+windows=Windows().sorted_by_class().filter_regular_type()
+windows.print()
+
+mouse=Mouse()
+for tile in Monitors().monitors[0].get_tiles(10, 10, True):
+    time.sleep(.05)
+    mouse.set_coords(tile.upper_left_x, tile.upper_left_y)
+
+time.sleep(1)
+if window.exists():
+    print("Window still exists.")
+window.close()
+if not window.exists():
+    print("window does not exists anymore.")
+
+# mouse.click(3)
+# mouse.right_click()
+# mouse.double_click()
+# mouse.triple_click()
