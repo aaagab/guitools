3a4f5ee360730d67e571c43cd79a49eef628ab2b
gabaaa <gabaaa.dev@gmail.com>
2021-07-25 20:22:41 -0500

    .gitignore updated

--- gpkgs/message/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# author: Gabriel Auger
-# version: 8.1.2
-# name: message
-# license: MIT
-
-__version__ = "8.1.2"
-
-from .dev.message import clear, error, info, success, warning
