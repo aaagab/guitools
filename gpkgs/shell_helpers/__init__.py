fe83281b5043dd7021628a092d1e7d92119b1ba1
gabaaa <not-set>
2021-07-25 20:22:41 -0500

    .gitignore updated

--- gpkgs/shell_helpers/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# author: Gabriel Auger
-# version: 5.4.1
-# name: shell_helpers
-# license: MIT
-__version__ = "5.4.1"
-
-from .dev.shell_helpers import cmd, cmd_get_value, cmd_prompt, cmd_devnull, get_cmd_str, rmtree
-from .gpkgs import message as msg
