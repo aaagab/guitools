b8132c97f0d22b076b28e36eb4227eac8b239b78
gabaaa <gabaaa.dev@gmail.com>
2021-07-25 22:06:29 -0500

    fixing import package gpkgs in __init__

--- __init__.py
+++ __init__.py
@@ -2,14 +2,22 @@
 # author: Gabriel Auger
 # name: guitools
 # license: MIT
-__version__= "2.0.0"
+__version__= "2.0.1"
+
+import sys
+import os
 
 from .dev.input import get_key_pressed
 from .dev.keyboards import Keyboard
 from .dev.monitors import Tile, Monitor, Monitors
 from .dev.mouses import Mouse
 from .dev.windows import Windows, Taskbars, Window, Regular_windows
-from .gpkgs.deps import deps
-from .gpkgs.xdginfo import xdginfo
-from .gpkgs.timeout import TimeOut
+
+if hasattr(sys.modules["__main__"], "__file__"):
+    direpa_main=os.path.dirname(os.path.abspath(sys.modules["__main__"].__file__))
+    direpa_file=os.path.dirname(os.path.realpath(__file__))
+    if direpa_file == direpa_main:
+        from .gpkgs.deps import deps
+        from .gpkgs.xdginfo import xdginfo
+        from .gpkgs.timeout import TimeOut
 
